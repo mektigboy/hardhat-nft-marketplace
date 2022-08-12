e31e548de488d21f0929d0cb21a02fa34dda1fde
mektigboy <mektigboy@protonmail.com>
2022-08-12 18:20:06 -0500

    Update deploy/01-deploy-nft-marketplace.js, deploy/02-deploy-basic-nft.js

---
+++ deploy/02-deploy-basic-nft.js
@@ -0,0 +1,27 @@
+const { developmentChains } = require("../helper-hardhat-config");
+const { network } = require("hardhat");
+const { verify } = require("../utils/verify");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const arguments = [];
+    const contract = await deploy("BasicNFT", {
+        args: arguments,
+        from: deployer,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        log("Verifying...");
+        await verify(contract.address, arguments);
+    }
+
+    log("--------------------------------------------------");
+};
+
+module.exports.tags = ["all", "basic-nft"];
